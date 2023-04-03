d3cfc7962c601998193a0db88cefd0f14ba98d7d
Cameron <cmuris4@gmail.com>
2023-04-03 15:21:23 +0000

    Updated with a way to print out the index position of students and students that only begin with the letter J

--- directory.rb
+++ directory.rb
@@ -9,9 +9,12 @@
      
      name = gets.chomp
      while !name.empty? do
+         if name[0] == "J"
          students << {name: name, cohort: :november}
          puts "Now we have #{students.count} students"
-        
+        else
+            puts "Enter a name that starts with J!"
+        end
         name = gets.chomp
     end
     
@@ -27,8 +30,8 @@ end
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
     
